feb534eb5c9f3f7746ca03589f9358ce597734c0
Chris Bednarczyk <chris@tourradar.com>
2024-11-03 22:56:13 +0100

    fix(frontend): Add translation to battery level

--- packages/frontend/components/NodeBattery/index.tsx
+++ packages/frontend/components/NodeBattery/index.tsx
@@ -2,9 +2,11 @@ import BatteryGauge from "react-battery-gauge";
 import * as React from "react";
 import { Tooltip } from "@mui/material";
 import { Node } from "../../../shared/Schemas/NodeSchema.ts";
+import { useTranslation } from "react-i18next";
 
 const NodeBattery: React.FC<{ node: Node }> = ({ node }) => {
   const metrics = node.device_metrics;
+  const { t } = useTranslation("nodes");
   if (!metrics || !metrics.battery_level) {
     return null;
   }
@@ -13,7 +15,10 @@ const NodeBattery: React.FC<{ node: Node }> = ({ node }) => {
 
   return (
     <Tooltip
-      title={`Battery level: ${level}%, Voltage: ${metrics.voltage?.toPrecision(2)}V`}
+      title={t("battery_level_description", {
+        level,
+        voltage: metrics.voltage?.toPrecision(2),
+      })}
     >
       <BatteryGauge
         value={level}
